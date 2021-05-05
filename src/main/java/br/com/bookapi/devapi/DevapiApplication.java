b191cb1d447b4005c864e275dd82936e03fd7d9d
hernani almeida <her_almeida23@hotmail.com>
2021-05-05 05:29:17 -0300

    Iniciando projeto start spring.io

---
+++ src/main/java/br/com/bookapi/devapi/DevapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.bookapi.devapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bookapi/devapi/DevapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bookapi.devapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
